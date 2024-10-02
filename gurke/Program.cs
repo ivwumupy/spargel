d482c1b36f0716931d4d8e366de8fe40fffbc080
Tianjiao Nie <ivwumupy@gmail.com>
2024-10-02 15:38:28 +0800

    [gurke] initial commit

---
+++ gurke/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Gurke
+{
+  public class Program
+  {
+    public static void Main(string[] args) {
+      Console.WriteLine("hello, gurke");
+    }
+  }
+}
---
+++ gurke/README.md
@@ -0,0 +1,8 @@
+# The Gurke Language
+
+```
+def main = do
+  print "hello, gurke!"
+def fib (n : i32) : i32 =
+  if n <= 1 then 1 else (fib (n-1)) + (fib (n-2))
+```
