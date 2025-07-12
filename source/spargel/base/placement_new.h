34d787100c8b8363817f63246ed076cdfab6407e
Tianjiao Nie <ivwumupy@gmail.com>
2025-07-12 20:06:24 +0800

    fix ci

--- source/spargel/base/placement_new.h
+++ source/spargel/base/placement_new.h
@@ -1,4 +1,6 @@
 #pragma once
 
-inline constexpr void* operator new(__SIZE_TYPE__ count, void* ptr) noexcept { return ptr; }
-inline constexpr void* operator new[](__SIZE_TYPE__ count, void* ptr) noexcept { return ptr; }
+#include <new>  // IWYU pragma: keep
+
+// inline constexpr void* operator new(__SIZE_TYPE__ count, void* ptr) noexcept { return ptr; }
+// inline constexpr void* operator new[](__SIZE_TYPE__ count, void* ptr) noexcept { return ptr; }
