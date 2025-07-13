0cf8b21d0b57bb02c872be18a6a72f6b339390df
Tianjiao Nie <ivwumupy@gmail.com>
2025-07-13 15:43:42 +0800

    remove unused

--- source/spargel/lang/lexer.cpp
+++
@@ -1,1 +0,0 @@
-#include <spargel/lang/lexer.h>
--- source/spargel/lang/lexer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <spargel/base/optional.h>
-#include <spargel/base/types.h>
-#include <spargel/base/vector.h>
-
-namespace spargel::lang {
-
-}
