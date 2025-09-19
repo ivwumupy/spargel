ea21251ec22008ebd8e5105616ae7de412c64f37
Tianjiao Nie <ivwumupy@gmail.com>
2025-09-19 14:02:51 +0800

    a first impl for alloc

--- source/spargel/base/result.h
+++ source/spargel/base/result.h
@@ -3,13 +3,13 @@
 #include "spargel/base/concept.h"
 
 namespace spargel::base {
-    namespace _expected {
+    namespace result_detail {
         template <typename E>
-        class Unexpected {
+        class Error {
         public:
             // TODO: U is not unexpected or in_place, and can construct E
             template <typename U>
-            constexpr explicit Unexpected(U&& u)
+            constexpr explicit Error(U&& u)
                 requires(ConstructibleFrom<E, U>)
                 : error_{forward<U>(u)} {}
 
@@ -22,31 +22,46 @@ namespace spargel::base {
             E error_;
         };
         template <typename E>
-        Unexpected(E) -> Unexpected<E>;
+        Error(E) -> Error<E>;
 
         template <typename T, typename E>
-        class Expected {
+        class Result {
         public:
             // actually this is value-initialization
-            constexpr Expected()
+            constexpr Result()
                 requires(DefaultInitializable<T>)
-                : value_{}, has_value_{false} {}
+                : value_{}, has_value_{true} {}
 
-            constexpr ~Expected() {
+            template <typename... Args>
+            constexpr Result(Args&&... args) : value_{forward<Args>(args)...}, has_value_{true} {}
+
+            template <typename U>
+            constexpr Result(Error<U> const& e) : error_{e.error()}, has_value_{false} {}
+            template <typename U>
+            constexpr Result(Error<U>&& e) : error_{move(e).error()}, has_value_{false} {}
+
+            constexpr ~Result() {
                 if (has_value_) {
-                    destruct_value();
+                    destructValue();
                 } else {
-                    destruct_error();
+                    destructError();
                 }
             }
 
+            constexpr bool is_error() const { return !has_value_; }
+
+            constexpr Error<E> error() && { return Error{move(error_)}; }
+
+            constexpr T& value() { return value_; }
+            constexpr T const& value() const { return value_; }
+
         private:
-            constexpr auto destruct_value() -> void {
+            constexpr auto destructValue() -> void {
                 if constexpr (!TriviallyDestructible<T>) {
                     value_.~T();
                 }
             }
-            constexpr auto destruct_error() -> void {
+            constexpr auto destructError() -> void {
                 if constexpr (!TriviallyDestructible<E>) {
                     error_.~E();
                 }
@@ -56,7 +71,7 @@ namespace spargel::base {
             E error_;
             bool has_value_;
         };
-    }  // namespace _expected
-    using _expected::Expected;
-    using _expected::Unexpected;
+    }  // namespace result_detail
+    using result_detail::Error;
+    using result_detail::Result;
 }  // namespace spargel::base
--- source/spargel/gpu/tlsf_allocator.h
+++ source/spargel/gpu/tlsf_allocator.h
@@ -1,9 +1,24 @@
 #pragma once
 
 #include "spargel/base/inline_array.h"
+#include "spargel/base/result.h"
 #include "spargel/base/types.h"
 
 namespace spargel::gpu {
+    namespace detail {
+        inline u8 countLeadingZero(u64 x) {
+            spargel_check(x != 0);
+            return static_cast<u8>(__builtin_clzl(x));
+        }
+        inline u8 countTrailingZero(u64 x) {
+            spargel_check(x != 0);
+            return static_cast<u8>(__builtin_ctzl(x));
+        }
+        inline u8 countTrailingZero(u32 x) {
+            spargel_check(x != 0);
+            return static_cast<u8>(__builtin_ctz(x));
+        }
+    }  // namespace detail
     // The TLSF Allocator
     //
     // One instance manages a sequence of memory blocks, which are binned into a two levels.
@@ -53,10 +68,56 @@ namespace spargel::gpu {
         // NOTE: The maximal size is `2 ** 64`.
         static constexpr u8 bin_count = 64 - linear_bits + 1;
 
+        // the minimal alignment is 8
+        static constexpr usize min_align = 1 << (linear_bits - subbin_bits);
+
         struct BinResult {
-            u16 bin_id;
+            u8 bin_id;
             u8 subbin_id;
+            usize index() const { return bin_id * subbin_count + subbin_id; }
         };
+
+        struct MemoryBlock {
+            u64 offset;
+            u64 size;
+            // The previous block in the free list.
+            MemoryBlock* prev_free;
+            MemoryBlock* next_free;
+            // The previous block that is physically adjacent.
+            MemoryBlock* prev_phys;
+            MemoryBlock* next_phys;
+            bool is_free;
+        };
+
+        struct AllocatedMemory {
+            MemoryBlock* block;
+        };
+
+        enum class AllocationError {
+            // `size` must be greater than `min_align`.
+            bad_size,
+            // `align` is not a power of two.
+            bad_align,
+            // cannot find a free block
+            out_of_block,
+            unknown,
+        };
+
+        base::Result<AllocatedMemory, AllocationError> allocate(usize size, usize align) {
+            if (size < min_align) {
+                return base::Error{AllocationError::bad_size};
+            }
+            if ((align & (align - 1)) != 0) {
+                return base::Error{AllocationError::bad_align};
+            }
+            auto bin = findBinWithFreeBlock(size);
+            if (bin.is_error()) {
+                return base::move(bin).error();
+            }
+            auto block = retrieveFreeBlock(bin.value());
+            // TODO: try to split block
+            return AllocatedMemory{block};
+        }
 
         // Find the smallest (sub)bin larger than the given size.
         //
@@ -68,7 +129,7 @@ namespace spargel::gpu {
         static BinResult binUp(usize size) {
             spargel_check(size != 0);
             // The unadjusted id in the range [8, 64)
-            auto bin_id = 63 - __builtin_clzl(size | min_alloc_size);
+            auto bin_id = 63 - detail::countLeadingZero(size | min_alloc_size);
             // log_2 of the size of each subbin in the chosen bin.
             auto log2_subbin_size = bin_id - subbin_bits;
             // size of each subbin - 1
@@ -84,13 +145,14 @@ namespace spargel::gpu {
             spargel_check(adjusted_bin_id < bin_count);
             spargel_check(adjusted_subbin_id < subbin_count);
 
-            return BinResult{u16(adjusted_bin_id), u8(adjusted_subbin_id)};
+            return BinResult{u8(adjusted_bin_id), u8(adjusted_subbin_id)};
         }
+
         // Similar to `binUp`. The only difference is that we round `size` down instead of up.
         static BinResult binDown(usize size) {
             spargel_check(size != 0);
             // The unadjusted id in the range [8, 64)
-            auto bin_id = 63 - __builtin_clzl(size | min_alloc_size);
+            auto bin_id = 63 - detail::countLeadingZero(size | min_alloc_size);
             // log_2 of the size of each subbin in the chosen bin.
             auto log2_subbin_size = bin_id - subbin_bits;
             auto subbin_id = size >> log2_subbin_size;
@@ -106,30 +168,73 @@ namespace spargel::gpu {
         }
 
         // Find a bin that contains a free block with enough space.
-        BinResult findBinWithFreeBlock(usize size) const {
+        base::Result<BinResult, AllocationError> findBinWithFreeBlock(usize size) const {
             auto [bin_id, subbin_id] = binUp(size);
             // Filter out subbins with enough space.
-            auto avail_subbin = subbin_bitmaps[bin_id] & (0xFFFFFFFF << subbin_id);
+            auto avail_subbin = subbin_bitmaps_[bin_id] & (0xFFFFFFFF << subbin_id);
             if (avail_subbin == 0) {
                 // In this case, no subbin in this bin contains a free block.
                 // Then we should find the next free bin.
-                auto avail_bin = bin_bitmap & (0xFFFFFFFF << (bin_id + 1));
+                auto avail_bin = bin_bitmap_ & (0xFFFFFFFF << (bin_id + 1));
                 if (avail_bin == 0) {
-                    spargel_panic("no free block");
+                    return base::Error{AllocationError::out_of_block};
                 }
-                bin_id = (u16)__builtin_ctzl(avail_bin);
-                avail_bin = subbin_bitmaps[bin_id];
+                bin_id = detail::countTrailingZero(avail_bin);
+                avail_bin = subbin_bitmaps_[bin_id];
                 spargel_check(avail_subbin != 0);
             }
-            subbin_id = (u8)__builtin_ctz(avail_subbin);
+            subbin_id = detail::countTrailingZero(avail_subbin);
             return BinResult{bin_id, subbin_id};
         }
 
     private:
+        // it is expected that the given bin is non-empty
+        MemoryBlock* retrieveFreeBlock(BinResult bin) {
+            auto index = bin.index();
+            auto block = free_blocks_[index];
+            spargel_check(block != nullptr);
+            spargel_check(block->is_free);
+            spargel_check(block->prev_free == nullptr);
+            auto next = block->next_free;
+            if (next != nullptr) {
+                free_blocks_[index] = next;
+                next->prev_free = nullptr;
+            } else {
+                // this subbin is now empty
+                subbin_bitmaps_[bin.bin_id] &= ~(u32(1) << bin.subbin_id);
+                if (subbin_bitmaps_[bin.bin_id] == 0) {
+                    // this bin is empty
+                    bin_bitmap_ &= ~(u32(1) << bin.bin_id);
+                }
+            }
+            return block;
+        }
+
+        void insertFreeBlock(MemoryBlock* block) {
+            auto bin = binDown(block->size);
+            usize list_id = bin.bin_id * subbin_count + bin.subbin_id;
+            addBlockToList(list_id, block);
+            bin_bitmap_ |= u32(1) << bin.bin_id;
+            subbin_bitmaps_[bin.bin_id] |= u32(1) << bin.subbin_id;
+        }
+
+        void addBlockToList(usize list_id, [[maybe_unused]] MemoryBlock* block) {
+            spargel_check(block != nullptr);
+            spargel_check(list_id < bin_count * subbin_count);
+            auto head = free_blocks_[list_id];
+            spargel_check(head->prev_free == nullptr);
+            block->next_free = head;
+            head->prev_free = block;
+            free_blocks_[list_id] = block;
+        }
+
         // NOTE: We support up to 64 bins.
-        u64 bin_bitmap;
+        static_assert(bin_count <= 64);
+        u64 bin_bitmap_;
         // NOTE: `subbin_count` is 32.
-        base::InlineArray<u32, bin_count> subbin_bitmaps;
+        static_assert(subbin_count <= 32);
+        base::InlineArray<u32, bin_count> subbin_bitmaps_;
+        base::InlineArray<MemoryBlock*, bin_count * subbin_count> free_blocks_;
     };
 
 }  // namespace spargel::gpu
